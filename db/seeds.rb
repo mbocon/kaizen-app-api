b5cea9485d8e185bf1af118bc1c69086bbad7c85
Michael Bocon <mikebocon@gmail.com>
2020-07-20 23:27:25 -1000

    implemented user auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+  { username: 'Bob', password: 'Bob' },
+  { username: 'Jill', password: 'Jill' }
+])
+
+Goal.create([
+  { title: 'Walk Dog', body: 'Walk the dog', user_id: 1 },
+  { title: 'Dishes', body: 'Do the dishes', user_id: 1 },
+  { title: 'Bills', body: 'Pay cable bill', user_id: 2 },
+  { title: 'Wash', body: 'Wash the car', user_id: 2 }
+])
