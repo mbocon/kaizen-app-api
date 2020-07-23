b7685bd69674aeb3b0125d087c6f653c0b9aa948
Michael Bocon <mikebocon@gmail.com>
2020-07-22 14:17:48 -1000

    changed user to username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   # POST /users/login
   def login
     user = User.find_by(username: params[:user][:username])
-    if user && user.authenticate(params[:user][:password])
+    if user && user.authenticate(params[:username][:password])
       token = create_token(user.id, user.username)
       render json: {status: 200, token: token, user: user}
     else
