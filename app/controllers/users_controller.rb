b3e5ef8a3f8aff1312f9f2e42031f7695a81c24c
Michael Bocon <mikebocon@gmail.com>
2020-07-22 14:35:36 -1000

    changed login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,10 +31,11 @@ class UsersController < ApplicationController
 
   # POST /users/login
   def login
-    user = User.find_by(username: params[:user][:username])
-    if user && user.authenticate(params[:username][:password])
+    @params = user_params
+    @user = User.find_by(username: @params[:user][:username])
+    if @user && @user.authenticate(@params[:user][:password])
       token = create_token(user.id, user.username)
-      render json: {status: 200, token: token, user: user}
+      render json: {status: 200, token: token, user: @user}
     else
       render json: {status: 401, message: "Unauthorized"}
     end
